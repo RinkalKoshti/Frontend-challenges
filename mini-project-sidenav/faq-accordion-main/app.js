8c9462b60e5b9e201f9abe93d710f49c297ec9aa
Rinkal Koshti <120006149+RinkalKoshti@users.noreply.github.com>
2024-03-26 10:58:35 +0530

    Delete mini-project-sidenav/faq-accordion-main directory

--- mini-project-sidenav/faq-accordion-main/app.js
+++
@@ -1,8 +0,0 @@
-const open = document.getElementsByClassName("ques");
-
-for(let i = 0; i < open.length; i++) {
-    open[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        document.getElementsByClassName("plus")[i].classList.toggle("active");
-    });
-}
