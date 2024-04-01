b6d42bebe75a4c0a3b3960775900709e19adff8b
Rinkal Koshti <rinkalkoshti2004@gmail.com>
2024-04-01 17:58:38 +0530

    added new project

---
+++ notifications-page-main/app.js
@@ -0,0 +1,31 @@
+const activeDiv = document.querySelectorAll(".active");
+const notifyNo = document.querySelector("header small");
+notifyNo.innerText = 3;
+
+activeDiv.forEach((div) => {
+    div.addEventListener("click", () => {
+        div.classList.remove("active");
+        let dot = div.querySelector("div.content > p > span.dot");
+        dot.classList.add("active"); 
+        let count = notifyNo.innerText;
+        if(div.getAttribute)
+        if (count == 3 || count == 2) {
+            count = count-1;
+            notifyNo.innerText = count;
+        } else {
+            count = 0;
+            notifyNo.innerText = count;
+        }
+    });
+});
+
+let allRead = document.querySelector("header p");
+
+allRead.addEventListener("click", () => {
+    activeDiv.forEach((div) => {
+        div.classList.remove("active");
+        let dot = div.querySelector("div.content > p > span.dot");
+        dot.classList.add("active"); 
+        notifyNo.innerText = 0;
+    })
+});
