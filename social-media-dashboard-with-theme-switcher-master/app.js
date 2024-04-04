62b92285daa0e90afae0bea2d9d455a69ef34704
Rinkal Koshti <rinkalkoshti2004@gmail.com>
2024-04-04 19:54:57 +0530

    project updated

--- social-media-dashboard-with-theme-switcher-master/app.js
+++ social-media-dashboard-with-theme-switcher-master/app.js
@@ -2,14 +2,7 @@ let toggleSwitch = document.querySelector("#checkbox");
 let headerBg = document.querySelector("header");
 let card = document.querySelectorAll(".card-light");
 let h2 = document.querySelector(".h2-light");
-// let smallCard = document.querySelectorAll(".small-card")
 
 toggleSwitch.addEventListener("click", () => {
-    console.log(card);
     document.body.classList.toggle("dark-body");
-    headerBg.classList.toggle("dark-header-top");
-    h2.classList.toggle("dark-h2");
-    card.forEach((card) => {
-        card.classList.toggle("dark-card");
-    }); 
 });
